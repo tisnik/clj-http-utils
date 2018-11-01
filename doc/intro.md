a826822447c91636fee87a64ab3c4b76df80ac77
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-01 09:19:59 +0100

    Stub for proper Clojure project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-http-utils
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_http_utils/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-http-utils.core-test
+  (:require [clojure.test :refer :all]
+            [clj-http-utils.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
