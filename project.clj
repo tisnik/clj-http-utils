0b6528f1abfae004922153624f9f86414690c23f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-25 08:26:54 +0200

    Updated (c) year in all sources

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2018  Pavel Tisnovsky
+;  (C) Copyright 2018, 2020  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -15,7 +15,8 @@
     :url "http://example.com/FIXME"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.8.0"]]
+    :dependencies [[org.clojure/clojure "1.8.0"]
+                   [ring/ring-core "1.3.2"]]
     :plugins [[test2junit "1.1.0"]
               [codox "0.8.11"]
               [lein-cloverage "1.0.7-SNAPSHOT"]])
