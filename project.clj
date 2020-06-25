8c6908ddd55e313905c58e6ee0a2254f5661a9b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-25 08:34:53 +0200

    Updated project file - new dependency

--- project.clj
+++ project.clj
@@ -16,6 +16,7 @@
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [ring/ring-core "1.3.2"]]
   :plugins [[test2junit "1.1.0"]
             [codox "0.8.11"]
