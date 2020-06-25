ff6b2087dadfcb019301da1888ed8ec4b1a32de5
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-25 08:27:42 +0200

    Reformatted project.clj by zprint

--- project.clj
+++ project.clj
@@ -11,12 +11,12 @@
 ;
 
 (defproject org.clojars.tisnik/clj-http-utils "0.1.0-SNAPSHOT"
-    :description "Utility functions for handling HTTP protocol."
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.8.0"]
-                   [ring/ring-core "1.3.2"]]
-    :plugins [[test2junit "1.1.0"]
-              [codox "0.8.11"]
-              [lein-cloverage "1.0.7-SNAPSHOT"]])
+  :description "Utility functions for handling HTTP protocol."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License",
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.3.2"]]
+  :plugins [[test2junit "1.1.0"]
+            [codox "0.8.11"]
+            [lein-cloverage "1.0.7-SNAPSHOT"]])
