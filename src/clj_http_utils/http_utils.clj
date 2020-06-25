132fafc6a05d2dca0284a0ccef1d77dcf2bb1d4e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-25 08:29:22 +0200

    Reformatted http_utils.clj by zprint

--- src/clj_http_utils/http_utils.clj
+++ src/clj_http_utils/http_utils.clj
@@ -11,7 +11,7 @@
 ;
 
 (ns clj-http-utils.http-utils
-    "Module that contains several HTTP-related utility functions.")
+  "Module that contains several HTTP-related utility functions.")
 
 
 (require '[ring.util.response    :as http-response])
@@ -19,39 +19,40 @@
 
 
 (defn return-file
-    "Create HTTP response containing content read from specified file.
-     Special value nil / HTTP response 404 is returned in case of any I/O error."
-    [^String prefix file-name content-type]
-    (let [file (new java.io.File prefix file-name)]
-        (log/info "Returning file " (.getAbsolutePath file))
-        (if (.exists file)
-            (-> (http-response/response file)
-                (http-response/content-type content-type))
-            (log/error "return-file(): can not access file: " (.getName file)))))
+  "Create HTTP response containing content read from specified file.
+   Special value nil / HTTP response 404 is returned in case of any I/O error."
+  [^String prefix file-name content-type]
+  (let [file (new java.io.File prefix file-name)]
+    (log/info "Returning file " (.getAbsolutePath file))
+    (if (.exists file)
+      (-> (http-response/response file)
+          (http-response/content-type content-type))
+      (log/error "return-file(): can not access file: " (.getName file)))))
 
 
 (defn cache-control-headers
-    "Update the response to contains all cache-control headers."
-    [response]
-    (-> response
-        (assoc-in [:headers "Cache-Control"] ["must-revalidate" "no-cache" "no-store"])
-        (assoc-in [:headers "Expires"] "0")
-        (assoc-in [:headers "Pragma"] "no-cache")))
+  "Update the response to contains all cache-control headers."
+  [response]
+  (-> response
+      (assoc-in [:headers "Cache-Control"]
+                ["must-revalidate" "no-cache" "no-store"])
+      (assoc-in [:headers "Expires"] "0")
+      (assoc-in [:headers "Pragma"] "no-cache")))
 
 
 (defn png-response
-    "Update the response with the content type valid for PNG images."
-    [image-data]
-    (-> image-data
-        (http-response/response)
-        (http-response/content-type "image/png")
-        cache-control-headers))
+  "Update the response with the content type valid for PNG images."
+  [image-data]
+  (-> image-data
+      (http-response/response)
+      (http-response/content-type "image/png")
+      cache-control-headers))
 
 
 (defn gif-response
-    "Update the response with the content type valid for GIF images."
-    [image-data]
-    (-> image-data
-        (http-response/response)
-        (http-response/content-type "image/gif")
-        cache-control-headers))
+  "Update the response with the content type valid for GIF images."
+  [image-data]
+  (-> image-data
+      (http-response/response)
+      (http-response/content-type "image/gif")
+      cache-control-headers))
